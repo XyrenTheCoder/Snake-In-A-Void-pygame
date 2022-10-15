5fdb3b9a31072b5c18ef4ddfebb068a2edb681c4
Kamilla Youver / Archisha <95626487+archisha69@users.noreply.github.com>
2022-10-15 17:54:15 +0800

    Update and rename pygame-snakes.py to snake-in-a-void.py

--- snake-in-a-void.py
+++ snake-in-a-void.py
@@ -1,4 +1,4 @@
-import pygame, time, random, os
+import pygame, random, os, os.path
 
 with open(".highscore.txt", "+r") as file:
     if os.name == 'nt': os.system("attrib +h .highscore.txt")
@@ -17,7 +17,7 @@ blue = pygame.Color(0,0,255)
 cyan = pygame.Color(0,255,255)
 
 pygame.init()
-pygame.display.set_caption('Now playing: snakes')
+pygame.display.set_caption('Now playing: Snake In A Void')
 game_window = pygame.display.set_mode((window_x, window_y))
 fps = pygame.time.Clock()
 
@@ -54,24 +54,36 @@ def game_over():
     f = open(".highscore.txt", 'a')
     f.write(f"{score}\n")
     f.close()
-    font = pygame.font.SysFont('courier', 50)
+    font = pygame.font.SysFont('segoe ui', 50)
     game_over_surface = font.render(f'Your score is : {str(score)}', True, white)
-    f = open('.highscore.txt')
-    lines = f.readlines()
-    scores = [line.strip() for line in lines]
-    highscore = max(scores)
+    f = open(".highscore.txt").read().splitlines()
+    q = list()
+    for i in f: q.append(int(i))
+    highscore = max(q)
     font2 = pygame.font.SysFont('courier', 30)
     game_over_sub = font2.render(f'Highest score record: {str(highscore)}', True, green)
+    font3 = pygame.font.SysFont('segoe ui', 20)
+    game_over_restart = font3.render('-Press R to restart, Press ESC to quit game-', True, blue)
     game_over_rect = game_over_surface.get_rect()
     game_over_rect2 = game_over_surface.get_rect()
+    game_over_rect3 = game_over_surface.get_rect()
     game_over_rect.midtop = (window_x/2, window_y/4)
-    game_over_rect2.midbottom = (window_x/2, window_y/2 + 100)
+    game_over_rect2.midbottom = (window_x/2-58, window_y/2+100)
+    game_over_rect3.bottomleft = (window_x/2-350, window_y/2+250)
+
     game_window.blit(game_over_surface, game_over_rect)
     game_window.blit(game_over_sub, game_over_rect2)
+    game_window.blit(game_over_restart, game_over_rect3)
     pygame.display.flip()
-    time.sleep(2)
-    pygame.quit()
-    quit()
+
+    for restart in pygame.event.get():
+        if restart.type == pygame.KEYDOWN:
+            if restart.key == pygame.K_r:
+                pygame.quit()
+                os.system(f"python {os.path.basename(__file__)}")
+            elif restart.key == pygame.K_ESCAPE: raise KeyboardInterrupt
+            else: pass
+        else: pass
 
 while True:
     for event in pygame.event.get():
@@ -80,6 +92,11 @@ while True:
             if event.key == pygame.K_DOWN: change_to = 'DOWN'
             if event.key == pygame.K_LEFT: change_to = 'LEFT'
             if event.key == pygame.K_RIGHT: change_to = 'RIGHT'
+            if event.key == pygame.K_ESCAPE: raise KeyboardInterrupt
+            if event.key == pygame.K_r:
+                pygame.quit()
+                os.system(f"python {os.path.basename(__file__)}")
+
     if change_to == 'UP' and direction != 'DOWN': direction = 'UP'
     if change_to == 'DOWN' and direction != 'UP': direction = 'DOWN'
     if change_to == 'LEFT' and direction != 'RIGHT': direction = 'LEFT'
@@ -101,7 +118,7 @@ while True:
         bomb_spawn = False
     elif snake_position[0] == wall_pos0[0] and snake_position[1] == wall_pos0[1] or snake_position[0] == wall_pos1[0] and snake_position[1] == wall_pos1[1] or snake_position[0] == wall_pos2[0] and snake_position[1] == wall_pos2[1] or snake_position[0] == wall_pos3[0] and snake_position[1] == wall_pos3[1]: game_over()
     else: snake_body.pop()
-    
+
     if not fruit_spawn:
         fruit_pos0 = [random.randrange(1, (window_x // 10)) * 10, random.randrange(1, (window_y // 10)) * 10]
         fruit_pos1 = [random.randrange(1, (window_x // 10)) * 10, random.randrange(1, (window_y // 10)) * 10]
@@ -137,6 +154,6 @@ while True:
     for block in snake_body[1:]:
         if snake_position[0] == block[0] and snake_position[1] == block[1]: game_over()
 
-    show_score(white, 'courier', 20)
+    show_score(white, 'segoe ui', 20)
     pygame.display.update()
     fps.tick(snake_speed)
